b8f291767b175e57938b029c65610a44c3c61e76
Pritesh Adulkar <adulkarritesh@gmail.com>
2021-03-23 23:09:59 +0530

    all files are commited

---
+++ src/main/java/Abstructcla/NewBranch.java
@@ -0,0 +1,9 @@
+package Abstructcla;
+
+public class NewBranch {
+
+	public void branch()
+	{
+		System.out.println("to got for 2nd commit");
+	}
+}
--- src/main/java/FB.java
+++
@@ -1,7 +0,0 @@
-
-public class FB {
-	public void branch()
-	{
-		System.out.println("to got for 3RD commit");
-	}
-}
