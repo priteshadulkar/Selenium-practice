d8d0bd1c8133cf4dd3aac96570b13d57372d5866
Pritesh Adulkar <adulkarritesh@gmail.com>
2021-03-23 23:13:51 +0530

    Revert "all files are commited"
    This reverts commit b8f291767b175e57938b029c65610a44c3c61e76.

--- src/main/java/Abstructcla/NewBranch.java
+++
@@ -1,9 +0,0 @@
-package Abstructcla;
-
-public class NewBranch {
-
-	public void branch()
-	{
-		System.out.println("to got for 2nd commit");
-	}
-}
---
+++ src/main/java/FB.java
@@ -0,0 +1,7 @@
+
+public class FB {
+	public void branch()
+	{
+		System.out.println("to got for 3RD commit");
+	}
+}
